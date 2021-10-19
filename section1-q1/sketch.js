fa6f9d6197492bcc635db1e4a55277ef78a19b78
pollo-en-el-techo <88078833+pollo-en-el-techo@users.noreply.github.com>
2021-10-19 12:10:55 +0900

    change section2-q1,2-q2

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -15,6 +15,7 @@ function setup(){
   strokeWeight(25);
   strokeCap(SQUARE);
   arc(100-25 , 100, 100, 100, QUARTER_PI+PI, QUARTER_PI +TWO_PI);
+// QUARTER_PI*5+PI　にしたらもっと分かりやすかったな、、くそう。。
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
+  createCanvas(200,200)
+  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    // 手順は、変数の宣言と動きの定義、色とか太さとか決めてあげる、描く。
+  if(i<5){
+    stroke('blue')
+  }
+  else{
+    stroke('red')
+  }
+  ellipse(100,100,i*10+10)
+  // やった～～！良い感じにできた！天才かもしれない！！
   }
 }
