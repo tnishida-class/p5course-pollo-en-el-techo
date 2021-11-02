f19079f4f20eb59ded06c70c9810e301d37f2788
pollo-en-el-techo <88078833+pollo-en-el-techo@users.noreply.github.com>
2021-11-02 11:16:38 +0900

    change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -3,14 +3,31 @@ let count;
 let cycle;
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(windowWidth, windowHeight);
   count = 0;
-  cycle = 100;
+  cycle = 200;
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight)
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+
+  if(keyIsDown(" ".charCodeAt(0))){
+    count = (count + 5) % cycle;
+  }
+  // cycleが100になったら余りは1に戻るよねってこと。
+  else{
+    count = (count + 1) % cycle;
+  }
+
+  if(count < 100){
+    ellipse(width / 2, height / 2, count);
+  }
+  else{
+    ellipse(width / 2, height / 2, 200 - count);
+  }
+
 }
